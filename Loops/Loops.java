8a2c18cb7a0e3769bbec56131e950d36fdc4bc01
Himadri Das <dashimadri1412@gmail.com>
2025-08-07 10:25:28 +0530

    change

--- Loops/Loops.java
+++ Loops/Loops.java
@@ -7,17 +7,17 @@ public class Loops
         //for loops
 
 
-        for(int counter=0;counter<10;counter=counter+1)
+        for(int counter=0;counter<3;counter=counter+1)
         {
             System.out.println("Hello World");
         }
         //coun++ --> coun=coun+1
-        for(int coun=0;coun<5;coun++)
+        for(int coun=0;coun<2;coun++)
         {
             System.out.println(coun);
         }
-        //print the numbers from 0 to 10
-        for(int i=0;i<11;i++)
+        //print the numbers from 0 to 5
+        for(int i=0;i<6;i++)
         {
             System.out.println(i);
         }
@@ -48,10 +48,12 @@ public class Loops
         System.out.println(sum);
 
         //print the table of a number input by the user
+        Scanner s=new Scanner(System.in);
+        int o=s.nextInt();
 
         for(int r=1;r<11;r++)
         {
-            System.out.println(r*n);
+            System.out.print(r*o+" ");
         }
 
     }
